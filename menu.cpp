ba2ee21e01fef8508eaa65ce3db55b5080d3de70
Coconutexploder <39191676+Coconutexploder@users.noreply.github.com>
2025-06-02 18:09:03 +0200

    Create menu.cpp v1

---
+++ menu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void afficherMenu()
+{
+    cout <<"Bienvenu dans GestionLog"<<endl;
+    cout <<"1. Afficher les logs sudo"<<endl;
+    cout <<"2. Afficher les logs SSH"<<endl;
+    cout <<"3. Quitter"<<endl;
+    cout <<"Votre choix"<<endl;
+}
