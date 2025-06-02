ab28c889aa473aec2d6a067549f1d0381dea5491
Coconutexploder <39191676+Coconutexploder@users.noreply.github.com>
2025-06-02 17:42:36 +0200

    Update main.cpp v2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,33 @@
 
 int main ()
 {
-  std::cout << "Bienvenue dans GestionLog" << std::endl;
+  int choix;
+  
+  cout <<"Bienvenu dans GestionLog"<<endl;
+  cout <<"1. Afficher les logs sudo"<<endl;
+  cout <<"2. Afficher les logs SSH"<<endl;
+  cout <<"3. Quitter"<<endl;
+  cout <<"Votre choix"<<endl;
+
+  if (choix == 1)
+  {
+    cout << "Vous avez choisi : Afficher les logs sudo" << endl;
+  }
+  
+  else if (choix == 2)
+  {
+    cout << "Vous avez choisi : Afficher les logs SSH" << endl;
+  }
+
+  else if (choix == 3)
+  {
+    cout << "Fermeture du programme" << endl;
+  }
+
+  else (choix == 1)
+  {
+    cout << "Choix invalide" << endl;
+  }
+
   return 0;
 }
